3d85b075861a1e1a41af8d1be06642cdd7f46987
chiendz11 <thuha25121976@gmail.com>
2024-12-15 23:07:34 +0700

    Add files via upload
    php files

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="\webproject\css\about.css">
+</head>
+<body>
+    <section>
+        <div class="box">
+            <h3>Về chúng tôi</h3>
+            <p>Đồ án này được hoàn thành bởi sự nhiệt tình của ChienBui với sự tham khảo<br>
+                <br>Bùi Anh Chiến
+                <br>MSSV: 23021490<br>
+            </p>
+        </div>
+    </section>
+</body>
+</html>
